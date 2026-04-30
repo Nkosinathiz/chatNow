1407395a7c8987c1b26c8dd48a8ffc17f89837fe
Nkosinathiz_ <ayceofunsound@gmail.com>
2026-04-30 12:36:31 +0200

    main

--- server.js
+++ server.js
@@ -1,39 +1,36 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const { text } = require('stream/consumers');
 
 const app = express();
-const Server = http.createServer(app);
+const server = http.createServer(app);
 const io = new Server(server);
-const PORT = 3000;
+const PORT = 3001;
 
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
-    console.log('A user connected');
+  console.log('A user connected');
 
-    //User joins with a username
-    socket.on('join', (username) => {
-        socket.username = username;
-        io.emit('message', { user: 'System', text: `${username} joined the chat...`});
+  // User joins with a username
+  socket.on('join', (username) => {
+    socket.username = username;
+    io.emit('message', { user: 'System', text: `${username} joined the chat` });
+  });
 
-    });
+  // User sends a message
+  socket.on('send_message', (text) => {
+    io.emit('message', { user: socket.username, text });
+  });
 
-    //User sends a message
-    socket.on('send_message', (text) => {
-        io.emit('message', { user: socket.username, text });
-
-    });
-
-    //User disconnects
-    socket.io('disconnect', () => {
-        if (socket.username) {
-            io.emit('message', { user: 'System', text: `${socket.username} left the chat`});
-        }
-    });
+  // User disconnects
+  socket.on('disconnect', () => {
+    if (socket.username) {
+      io.emit('message', { user: 'System', text: `${socket.username} left the chat ` });
+    }
+  });
 });
 
 server.listen(PORT, () => {
-    console.log('ChatNow running at http://localhost:${PORT}');
+  console.log(`Chat app running at http://localhost:${PORT}`);
 });
